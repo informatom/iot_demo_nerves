b62ad9f4470b630ab69df5d7a181973a0f79849f
Stefan Haslinger <mail@stefan-haslinger.at>
2022-03-21 13:54:54 +0100

    bugfix: wrong sensor addressed

--- sensor_hub/lib/sensor_hub/application.ex
+++ sensor_hub/lib/sensor_hub/application.ex
@@ -1,6 +1,5 @@
 defmodule SensorHub.Application do
   use Application
-
   alias SensorHub.Sensor
 
   @impl true
@@ -20,7 +19,7 @@ defmodule SensorHub.Application do
     [
       {SGP30, []},
       {BMP280, [i2c_address: 0x77, name: BMP280]},
-      {VEML6030, %{}},
+      {VEML6075, %{}},
       {Finch, name: WeatherTrackerClient},
       {
         Publisher,
@@ -33,7 +32,7 @@ defmodule SensorHub.Application do
   end
 
   defp sensors do
-    [Sensor.new(BMP280), Sensor.new(VEML6030), Sensor.new(SGP30)]
+    [Sensor.new(BMP280), Sensor.new(VEML6075), Sensor.new(SGP30)]
   end
 
   defp weather_tracker_url do
