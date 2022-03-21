2c48ab46d16876c9739fa59dc50452b207f4869c
Stefan Haslinger <mail@stefan-haslinger.at>
2022-03-21 13:44:36 +0100

    supervision tree setup

--- sensor_hub/lib/sensor_hub/application.ex
+++ sensor_hub/lib/sensor_hub/application.ex
@@ -15,7 +15,19 @@ defmodule SensorHub.Application do
   end
 
   def children(_target) do
-    []
+    [
+      {SGP30, []},
+      {BMP280, [i2c_address: 0x77, name: BMP280]},
+      {VEML6030, %{}},
+      {Finch, name: WeatherTrackerClient},
+      {
+        Publisher,
+        %{
+          sensors: sensors(),
+          weather_tracker_url: weather_tracker_url()
+        }
+      }
+    ]
   end
 
   def target() do
